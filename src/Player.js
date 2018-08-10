ee4cb8c4b8094094fb0983bd320e0dd15355b9ca
Ivan <ivanmtta@gmail.com>
2018-08-10 01:03:27 -0600

    Added Player Object file

---
+++ src/Player.js
@@ -0,0 +1,59 @@
+function Player(){
+	this.size = 46;
+	this.x = frame.width / 2 - this.size / 2;
+	this.y = frame.height / 2 - this.size / 2;
+	this.angle = 0;
+	this.velocity = 5;
+	this.thrusting = false;
+	this.thrust = {
+		x: 0,
+		y: 0
+	};
+	this.playerNormal = new Image();
+	this.playerNormal.src = "img/player.png";
+	this.playerThrusting = new Image();
+	this.playerThrusting.src = "img/playerThrust.png";
+
+	this.update = function(){
+		if(this.thrusting){
+			this.thrust.x += this.velocity * Math.cos(this.angle) / FPS;
+			this.thrust.y -= this.velocity * Math.sin(this.angle) / FPS;
+		}
+		else{
+			this.thrust.x -= FRICTION * this.thrust.x / FPS;
+			this.thrust.y -= FRICTION * this.thrust.y / FPS;
+		}
+		this.x += this.thrust.x;
+		this.y += this.thrust.y;
+		this.handleEdges();
+	}
+
+	this.draw = function(){
+		graphics.save();
+		graphics.translate(this.x - this.size / 2, this.y - this.size / 2);
+		graphics.rotate(this.angle);
+		graphics.translate(-this.x - this.size / 2, -this.y - this.size / 2);
+		if(player.thrusting){
+			graphics.drawImage(this.playerThrusting, this.x, this.y, this.size, this.size);
+		}
+		else{
+			graphics.drawImage(this.playerNormal, this.x, this.y, this.size, this.size);
+		}
+		graphics.restore();
+	}
+
+	this.handleEdges = function(){
+		if(this.x + this.size / 2 < 0){
+			this.x = frame.width + this.size / 2;
+		}
+		if(this.x - this.size / 2 > frame.width){
+			this.x = 0 - this.size / 2;
+		}
+		if(this.y + this.size / 2 < 0){
+			this.y = frame.height + this.size / 2;
+		}
+		if(this.y - this.size / 2 > frame.height){
+			this.y = 0 - this.size / 2;
+		}
+	}
+}
